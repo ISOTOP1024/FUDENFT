3bfc17ed6a4ef0da9e9189cec8e1f3925d61b16d
ISOTOP1024 <132178562+ISOTOP1024@users.noreply.github.com>
2023-04-30 00:00:28 -0400

    Update DestructionVote.sol

--- DestructionVote.sol
+++ DestructionVote.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@pancakeswap/pancake-swap-lib/contracts/token/BEP20/IBEP20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
