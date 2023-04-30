40f0713273333df079c2298bcfc274176bd2c1a5
ISOTOP1024 <132178562+ISOTOP1024@users.noreply.github.com>
2023-04-29 23:31:38 -0400

    Update FudeToken.sol

--- FudeToken.sol
+++ FudeToken.sol
@@ -4,22 +4,29 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract FudeToken is ERC20, Ownable {
     using SafeMath for uint256;
 
     uint256 public constant TOTAL_SUPPLY = 100000000 * 10**18;
+    uint256 public constant TARGET_PRICE = 1000 * 10**18;
+    uint256 public constant PERCENTAGE_CHANGE = 1;
+
+    AggregatorV3Interface internal priceFeed;
 
     mapping(address => bool) private _frozenAccounts;
 
     event FrozenFunds(address target, bool frozen);
 
-    constructor() ERC20("FudeToken", "FUDE") {
+    constructor(AggregatorV3Interface _priceFeed) ERC20("FudeToken", "FUDE") {
         _mint(msg.sender, TOTAL_SUPPLY);
+        priceFeed = _priceFeed;
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount);
+        uint256 adjustedAmount = adjustAmount(amount);
+        _mint(to, adjustedAmount);
     }
 
     function burn(address from, uint256 amount) public onlyOwner {
@@ -33,6 +40,20 @@ contract FudeToken is ERC20, Ownable {
 
     function isFrozen(address target) public view returns (bool) {
         return _frozenAccounts[target];
+    }
+
+    function getLatestPrice() public view returns (int) {
+        (, int price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    function adjustAmount(uint256 amount) private view returns (uint256) {
+        int price = getLatestPrice();
+        if (price >= int(TARGET_PRICE)) {
+            return amount.add(amount.mul(PERCENTAGE_CHANGE).div(10000));
+        } else {
+            return amount.sub(amount.mul(PERCENTAGE_CHANGE).div(10000));
+        }
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
